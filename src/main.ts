236a4c6730f13e21d4b8b7881a2dbc063d5966cf
Eric Haemmerle <eric.haemmerle@intension.de>
2022-01-27 09:57:05 +0100

    VBTN-7 Removal of e2e Tests and Introduction PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import PrimeVue from "primevue/config";
+import "/node_modules/primeflex/primeflex.css";
+import "primevue/resources/primevue.min.css"; //core css
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App).use(store);
+
+app.use(PrimeVue);
+
+app.mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,7 @@
 import { createStore } from "vuex";
+import { installedDB } from "./ORM-Stores/index";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
   modules: {},
+  plugins: [installedDB],
 });
