252e43cc1765ae91b625c66e55e33d59b289ee6f
Eric Haemmerle <eric.haemmerle@intension.de>
2022-01-27 09:57:52 +0100

    VBTN-7 first experimets with Web Components

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import PrimeVue from "primevue/config";
 import "/node_modules/primeflex/primeflex.css";
 import "primevue/resources/primevue.min.css";
 
+/* 
 const app = createApp(App).use(store);
 
 app.use(PrimeVue);
 
-app.mount("#app");
+app.mount("#app"); */
