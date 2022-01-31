e8b1cb280a9ea5bff2cbac4b2e6ba58f7093a81f
Eric Haemmerle <eric.haemmerle@intension.de>
2022-01-31 11:34:08 +0100

    VBTN-8 import PrimeLibs directly

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -13,8 +13,8 @@ export default defineComponent({
 </script>
 
 <style>
-@import url("https://unpkg.com/primevue/resources/primevue.min.css");
 @import url("https://unpkg.com/primeflex@3.1.0/primeflex.css");
-@import url("https://unpkg.com/primevue/resources/themes/tailwind-light/theme.css");
 </style>
 <style lang="scss" src="@/assets/scss/globals.scss"></style>
+<style src="primevue/resources/primevue.min.css"></style>
+<style src="primevue/resources/themes/tailwind-light/theme.css"></style>
