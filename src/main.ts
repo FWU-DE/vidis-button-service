eacf9abe5c8ab88272848285e4e8b032242aafb2
Eric Haemmerle <eric.haemmerle@intension.de>
2022-01-31 09:25:59 +0100

    VBTN-8 correct merge mistakes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
 import { defineCustomElement } from "vue";
 import App from "./App.ce.vue";
 
-import "/node_modules/primeflex/primeflex.css";
-import "primevue/resources/primevue.min.css";
-
-/* 
-const app = createApp(App).use(store);
-
-app.use(PrimeVue);
-
-app.mount("#app");
+customElements.define("vidis-login", defineCustomElement(App));
