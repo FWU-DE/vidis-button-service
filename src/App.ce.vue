31e22c9d96d7cf158a6bc527d461c1f61b5c20dd
telion2 <30474592+telion2@users.noreply.github.com>
2022-03-22 09:31:54 +0100

    VBTN-45 add entrance section (#11)
    * VBTN-36: added version to cdn links (#12)
    
    * VBTN-36: added version to cdn folders
    
    * VBTN-36: added version to cdn folders
    
    * VBTN-42: automatically create rc tag (#13)
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-7 Initialize Vue Base Project
    
    * VBTN-7 Removal of e2e Tests and Introduction PrimeVue
    
    * VBTN-7 first experimets with Web Components
    
    * VBTN-8 Add build command, building project as webcomponents
    
    * VBTN-8 fix CICD
    
    * VBTN-40 Rewrite Webcomponent, Add Error Listeners
    
    * VBTN-40 logs Errors as Errors
    
    * VBTN-40 Review Changes
    
    * VBTN-45 Add Entrance Section & Tests
    
    * VBTN-45 convert test files to ts
    
    * VBTN-45 Remove local Testapplications
    
    * VBTN-45 add fonts
    
    * VBTN-45 correct font paths.
    
    * VBTN-45 update Version
    
    * VBTN-45 update version
    
    Co-authored-by: mmartynenko1 <98532836+mmartynenko1@users.noreply.github.com>
    Co-authored-by: Eric Haemmerle <eric.haemmerle@intension.de>

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -1,20 +1,26 @@
 <template>
-  <vidisLogin msg="name" />
+  <component :is="entranceComponent" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import vidisLogin from "@/components/entranceButton/entranceButton.ce.vue";
+import entrance from "@/components/entrance/entrance.ce.vue";
+import smallEntrance from "@/components/entrance/smallEntrance.ce.vue";
 export default defineComponent({
   name: "vidis-login",
-  components: { vidisLogin },
+  components: { entrance, smallEntrance },
+  inject: {
+    size: {
+      default: "L",
+    },
+  },
+  computed: {
+    entranceComponent(): string {
+      if (this.size === "L" || this.size === "M") return "entrance";
+      else if (this.size === "S") return "smallEntrance";
+      else return "entrance";
+    },
+  },
 });
 </script>
-
-<style>
-@import url("https://unpkg.com/primeflex@3.1.0/primeflex.css");
-</style>
-<style lang="scss" src="@/assets/scss/globals.scss"></style>
-<style src="primevue/resources/primevue.min.css"></style>
-<style src="primevue/resources/themes/tailwind-light/theme.css"></style>
---
+++ src/components/entrance/smallEntrance.ce.vue
@@ -0,0 +1,32 @@
+<template>
+  <entryButton @clicked="toggleDialog(true)" />
+  <idpDialog
+    :visible="showDialog"
+    class="p-dialog-maximized"
+    header="Header"
+    footer="Footer"
+    @closed="showDialog = false"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import idpDialog from "@/components/idpSelection/idpDialog.ce.vue";
+import entryButton from "@/components/entrance/entryButton.ce.vue";
+export default defineComponent({
+  name: "smallEntrance",
+  props: {},
+  components: { entryButton, idpDialog },
+  data() {
+    return {
+      showDialog: false,
+    };
+  },
+  methods: {
+    toggleDialog(mode?: boolean) {
+      this.showDialog = mode ?? false;
+    },
+  },
+});
+</script>
