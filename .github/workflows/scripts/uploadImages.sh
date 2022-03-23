97888a27601fb02517e59f35fe5793ceb5446d32
Mariia Martynenko <mmartynenko@intension.de>
2022-03-23 09:57:02 +0100

    VBTN-52: added images and fonts for rc

--- .github/workflows/scripts/uploadImages.sh
+++ .github/workflows/scripts/uploadImages.sh
@@ -2,11 +2,11 @@
 echo "Uploading images..."
 for file in dist/img/*
 do
-    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-snapshot/img/;
+    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/img/;
 done;
 
 echo "Uploading fonts..."
 for file in dist/fonts/*
 do
-    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-snapshot/fonts/;
+    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/fonts/;
 done;
