1aaa2bb0dcfe2610d0805ea78e0f464e84f8d128
Mariia Martynenko <mmartynenko@intension.de>
2022-03-25 08:53:10 +0100

    VBTN-52: test release pipeline with assets

--- .github/workflows/scripts/uploadReleaseAssets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Uploading images and fonts..."
-find * -type f > assets.list
-
-while IFS="" read -r file || [ -n "$file" ]
-do
-    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
-done < assets.list
