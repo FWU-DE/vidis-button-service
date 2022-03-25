32f3e775f4743e4de71252173e9effc7942d76a7
Mariia Martynenko <mmartynenko@intension.de>
2022-03-25 10:07:36 +0100

    VBTN-52: cleanup

--- .github/workflows/scripts/uploadImagesAndFonts.sh
+++ .github/workflows/scripts/uploadImagesAndFonts.sh
@@ -6,6 +6,6 @@ find * -type f > assets.list
 
 while IFS="" read -r file || [ -n "$file" ]
 do
-    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
-    curl -v -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
 done < assets.list
