7a9bbd98932f434952a6768a5766b38abd64f129
mmartynenko1 <98532836+mmartynenko1@users.noreply.github.com>
2022-03-28 11:15:52 +0200

    VBTN-52: upload fonts and svgs (#16)
    * VBTN-41: build and publish rc (#14)
    
    * VBTN-43: release pipeline (#15)
    
    * VBTN-43: release pipeline
    
    * VBTN-43: release pipeline testing tags
    
    * VBTN-45 add entrance section (#11)
    
    * VBTN-36: added version to cdn links (#12)
    
    * VBTN-36: added version to cdn folders
    
    * VBTN-36: added version to cdn folders
    
    * VBTN-42: automatically create rc tag (#13)
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-42: automatically create rc tag if version increased
    
    * VBTN-7 Initialize Vue Base Project
    
    * VBTN-7 Removal of e2e Tests and Introduction PrimeVue
    
    * VBTN-7 first experimets with Web Components
    
    * VBTN-8 Add build command, building project as webcomponents
    
    * VBTN-8 fix CICD
    
    * VBTN-40 Rewrite Webcomponent, Add Error Listeners
    
    * VBTN-40 logs Errors as Errors
    
    * VBTN-40 Review Changes
    
    * VBTN-45 Add Entrance Section & Tests
    
    * VBTN-45 convert test files to ts
    
    * VBTN-45 Remove local Testapplications
    
    * VBTN-45 add fonts
    
    * VBTN-45 correct font paths.
    
    * VBTN-45 update Version
    
    * VBTN-45 update version
    
    Co-authored-by: mmartynenko1 <98532836+mmartynenko1@users.noreply.github.com>
    Co-authored-by: Eric Haemmerle <eric.haemmerle@intension.de>
    
    * VBTN-52: added image upload
    
    * VBTN-52: added image upload
    
    * VBTN-52: allowed CORS for fonts
    
    * VBTN-52: added images and fonts for rc
    
    * VBTN-52: added images and fonts for rc
    
    * VBTN-52: test downloading images and fonts
    
    * VBTN-52: download fonts test
    
    * VBTN-52: test release pipeline
    
    * VBTN-52: test release pipeline with assets
    
    * VBTN-52: test release pipeline with assets
    
    * VBTN-52: cleanup
    
    * VBTN-52: cleanup
    
    Co-authored-by: telion2 <30474592+telion2@users.noreply.github.com>
    Co-authored-by: Eric Haemmerle <eric.haemmerle@intension.de>

---
+++ .github/workflows/scripts/uploadImagesAndFonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Uploading images and fonts..."
+rm dist/demo.html dist/*.min.js dist/*.map dist/*.common.js
+cd dist
+find * -type f > assets.list
+
+while IFS="" read -r file || [ -n "$file" ]
+do
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
+done < assets.list
