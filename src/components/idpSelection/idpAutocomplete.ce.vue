1283ef067bd609f64687b72f515d4cc32b6083ba
Mariia Martynenko <mmartynenko@intension.de>
2022-04-08 16:30:17 +0200

    VBTN-18: added grouping

--- src/components/idpSelection/idpAutocomplete.ce.vue
+++ src/components/idpSelection/idpAutocomplete.ce.vue
@@ -1,6 +1,7 @@
 <template>
   <span class="p-fluid">
     <AutoComplete
+      dropdown
       forceSelection
       v-model="selectedIdP"
       :suggestions="filteredIdps"
@@ -23,6 +24,25 @@
         </div>
       </template>
     </AutoComplete>
+
+    <AutoComplete
+      dropdown
+      forceSelection
+      v-model="selectedCity"
+      :suggestions="filteredCities"
+      @item-select="emitToParent"
+      @complete="searchGroupedIdps($event)"
+      field="name"
+      optionGroupLabel="label"
+      optionGroupChildren="items"
+      placeholder="z. B. Bundesland, Stadt, Email, Schule, ..."
+    >
+      <template #optiongroup="slotProps">
+        <div class="autocomplete-small-item">
+          <div>{{ slotProps.item.label }}</div>
+        </div>
+      </template>
+    </AutoComplete>
   </span>
 </template>
 
@@ -32,6 +52,8 @@ import AutoComplete from "primevue/autocomplete";
 import IdP from "@/store/ORM-Stores/models/idps";
 import axios from "axios";
 import schoolSvg from "@/assets/svgs/school_icon.svg";
+import _ from "lodash";
+import { FilterService, FilterMatchMode } from "primevue/api";
 
 export default defineComponent({
   name: "idp-dialog",
@@ -45,15 +67,23 @@ export default defineComponent({
       suggestions: [],
       availableIdps: [],
       schoolSvg,
+      selectedCity: null,
+      filteredCities: null,
+      notGroupedIdps: null,
     };
   },
   async created() {
     await this.loadIdps();
+    this.getListOfStates();
+    this.groupIdps();
   },
 
   computed: {
     idpsInStore() {
       return IdP.all();
+    },
+    listOfStates() {
+      return this.getListOfStates();
     },
     schoolLogo() {
       return schoolSvg;
@@ -63,11 +93,9 @@ export default defineComponent({
     async loadIdps() {
       try {
         let res = await axios.get(
-          "https://fwu-nexus.intension.eu/repository/vidis-cdn/data/idps_test3.json"
+          "https://fwu-nexus.intension.eu/repository/vidis-cdn/data/idps_test4.json"
         );
         this.availableIdps = res.data.idp;
-        console.log("!!!!!!!!!!");
-        console.log(this.availableIdps);
         IdP.insert({
           data: this.availableIdps,
         });
@@ -79,17 +107,57 @@ export default defineComponent({
       this.$emit("emitSelectedIdp", this.selectedIdP);
     },
     searchIdps(event: any) {
-      setTimeout(() => {
-        if (!event.query.trim().length) {
-          this.filteredIdps = [...this.idpsInStore];
-        } else {
-          this.filteredIdps = this.idpsInStore.filter(
-            (idp: { name: string }) => {
-              return idp.name.toLowerCase().includes(event.query.toLowerCase());
-            }
-          );
+      if (!event.query.trim().length) {
+        this.filteredIdps = [...this.idpsInStore];
+      } else {
+        this.filteredIdps = this.idpsInStore.filter((idp: { name: string }) => {
+          return idp.name.toLowerCase().includes(event.query.toLowerCase());
+        });
+      }
+    },
+
+    searchGroupedIdps(event: any) {
+      let query = event.query;
+      let filteredCities = [];
+
+      for (let state of this.finalGroupedIdps) {
+        let filteredItems = FilterService.filter(
+          state.items,
+          ["name"],
+          query,
+          FilterMatchMode.CONTAINS
+        );
+        if (filteredItems && filteredItems.length) {
+          filteredCities.push({ ...state, ...{ items: filteredItems } });
         }
-      }, 5);
+      }
+      this.filteredCities = filteredCities;
+    },
+    getIdpsForState(state: string) {
+      let idpsForParticularState = _.filter(
+        this.idpsInStore,
+        function (el: any) {
+          return el.address.state === state;
+        }
+      );
+      return idpsForParticularState;
+    },
+    getListOfStates() {
+      let statesList = this.idpsInStore.map(
+        (value: any) => value.address.state
+      );
+      return _.uniq(statesList);
+    },
+    groupIdps() {
+      let finalGroupedIdps = [];
+      for (let state of this.getListOfStates()) {
+        finalGroupedIdps.push({
+          ...{ label: state },
+          ...{ items: this.getIdpsForState(state) },
+        });
+      }
+      this.finalGroupedIdps = finalGroupedIdps;
+      return finalGroupedIdps;
     },
   },
 });
--- tests/unit/components/idpSelection/idpAutocomlpete.spec.ts
+++ tests/unit/components/idpSelection/idpAutocomlpete.spec.ts
@@ -1,8 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
 import IdPAutocomplete from "@/components/idpSelection/idpAutocomplete.ce.vue";
+import store from "@/store/index";
 import IdPs from "@/store/ORM-Stores/models/idps";
 import searchIcon from "@/assets/svgs/search.svg";
 import exampleIdps from "./idpsExample.json";
+import storeIdps from "./store.json";
 
 
 import { messages } from "@/languages/i18nPlugin";
@@ -13,17 +15,19 @@ describe("IdPAutocomplete", () => {
                 mocks: {
                     $t: (key: string) => messages["de"][key],
                 },
+                store
         });
     });
 
-    describe("created", () => {
+    describe("methods", () => {
         /**
         * GIVEN: Component
         * WHEN: Component is created
         * THEN: created was called and idps were loaded
             */
-        test("idps are being loaded", async () => {
+        test("idps are being loaded", async() => {
             await IdPAutocompleteWrapper.vm.loadIdps();
+            expect(IdPAutocompleteWrapper.vm.idpsInStore).toHaveLength(4);
         });
 
         describe("computed", () => {
