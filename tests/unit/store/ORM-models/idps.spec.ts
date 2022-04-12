05fc220cf5fc61c95b4f698e92cc5fb740d597d3
Mariia Martynenko <mmartynenko@intension.de>
2022-04-12 13:41:29 +0200

    VBTN-18: test tests

--- tests/unit/store/ORM-models/idps.spec.ts
+++ tests/unit/store/ORM-models/idps.spec.ts
@@ -1,6 +1,5 @@
 import IdPs from "@/store/ORM-Stores/models/idps";
 
 describe("Test ORM-Model: IdPs", () => {
-  test("that creating and retrieving idps works", () => {
-  });
+  test("that creating and retrieving idps works", () => {});
 });
