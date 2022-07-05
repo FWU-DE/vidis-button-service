215ac0088c7d892abec2caf305c590a871b8a8e4
Norbert Varzariu <nvarzariu@intension.de>
2022-07-05 13:32:25 +0200

    add selenium test
    Signed-off-by: Norbert Varzariu <nvarzariu@intension.de>

---
+++ tests/selenium/src/split/4th button VIDIS size S cookie  false/2nd page zurük button test.js
@@ -0,0 +1,20 @@
+const { Builder, until, By, Key } = require ("selenium-webdriver");
+const { Action } = require("selenium-webdriver/lib/input");
+const assert = require ("assert");
+    
+    
+async function firefoxVIDIS(){
+
+//select the browser
+let driver = await new Builder().forBrowser("firefox").build();
+
+//navigate to website
+await driver.get("https://tp.fwu.intension.eu/?version=latest")
+
+//click on first VIDIS button
+await driver.findElement(By.css("vidis-login:not([size]) button.entrance-button")).click();
+
+//verify "zuruck" button
+await driver.findElement(By.className("p-button p-component p-button-link backButton")).click(); 
+
+}
