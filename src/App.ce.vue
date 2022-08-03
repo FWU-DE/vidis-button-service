b99ab60bd2224315f28d6afb1a73b450d3527461
Eric Haemmerle <eric.haemmerle@intension.de>
2022-08-03 13:53:01 +0200

    VBTN-92 make idphint directly selectable by the service provider

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -15,6 +15,7 @@ export default defineComponent({
       return this.$store.getters.size;
     },
     entranceComponent(): string {
+      console.log("entranceComponent size", this.size);
       if (this.size === "L" || this.size === "M") return "entrance";
       else if (this.size === "S") return "smallEntrance";
       else return "entrance";
