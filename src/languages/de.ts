12b81d146c488c846552d5fc2160361f3f9da2c1
telion2 <30474592+telion2@users.noreply.github.com>
2022-09-12 09:27:01 +0200

    VBTN-71 update texts (#61)
    * VBTN-71 update texts
    
    * vBTN-71 fix tests
    
    Co-authored-by: Eric Haemmerle <eric.haemmerle@intension.de>

--- src/languages/de.ts
+++ src/languages/de.ts
@@ -2,7 +2,7 @@ export default {
   general: {
     back: "zurück",
     branding_accessibility: "VIDIS-Logo", //If a user is blind, this text replaces the big branding logo
-    secureLoginLabel: "Sicherer Login",
+    secureLoginLabel: "Sichere Anmeldung",
     help: "Wie funktioniert es?",
     dataprivacy: "Datenschutz",
     terms: "Nutzungsbedingungen",
@@ -12,13 +12,13 @@ export default {
   entrance: {
     help: "Wie funktioniert es?", //displayed on the top right corner and opens a section that displays help
     banner: "Sicher & einfach über", //displayed text on the top left corner
-    button: "Anmelden mit deinem Schulaccount", //Text displayed on the button that opens the dialog and start the base user journey
+    button: "Anmelden", //Text displayed on the button that opens the dialog and start the base user journey
     buttonSelectedIdp: "Weiter mit ",
-    cookieSelectIdp: "Mit anderem Nutzer / Schulportal einloggen",
+    cookieSelectIdp: "Schulportal ändern",
   },
   idp: {
-    button: "Secure Login in ",
-    placeholder: "z. B. Bundesland, Stadt, Email, Schule, ...",
+    button: "Sichere Anmeldung über ",
+    placeholder: "z. B. Bundesland, Stadt, E-Mail, Schule, ...",
     other: "Sonstige",
     noResult: "Leider haben wir zu Ihren Suchkriterien",
     noResult2: "keine passenden Ergebnisse gefunden.",
@@ -29,14 +29,14 @@ export default {
     closeAlt: "Hilfe Schließen",
     initiativeFrom: "Eine Initiative von",
     heading1: "Wie funktioniert’s?",
-    heading2: "Sicherer und einfacher Login über VIDIS",
+    heading2: "Sicherer und einfache Anmeldung über VIDIS",
     footerTitle: "Was macht VIDIS",
     footerText:
-      "VIDIS bietet zukünftig einen einzigen Login für Online-Dienste in Schule und Ausbildung. Mit VIDIS wird der Zugang zu Online-Diensten in Schule und Unterricht für Schülerinnen und Schüler sowie Lehrkräfte deutlich vereinfacht. Es wird einen Login für eine Vielzahl von Bildungsangeboten geben, damit die vielen unterschiedlichen Accounts und das häufige „Passwort zurücksetzen“ für alle ein Ende haben. Darüber hinaus schützt es die digitalen Identitäten und gewährleistet einen abgesicherten Zugang (rechts- & datenschutzkonform) zu Online-Diensten.",
+      "VIDIS bietet zukünftig eine einzige Anmeldung für Online-Dienste in Schule und Ausbildung. Mit VIDIS wird der Zugang zu Online-Diensten in Schule und Unterricht für Schülerinnen und Schüler sowie Lehrkräfte deutlich vereinfacht. Es wird einen Login für eine Vielzahl von Bildungsangeboten geben, damit die vielen unterschiedlichen Accounts und das häufige „Passwort zurücksetzen“ für alle ein Ende haben. Darüber hinaus schützt es die digitalen Identitäten und gewährleistet einen abgesicherten Zugang (rechts- & datenschutzkonform) zu Online-Diensten.",
     footerPortals: "Teilnehmende Länder, Schulträger und Schulen",
     footerPortalRedirect: "Jetzt mehr erfahren",
     steps: {
-      title1: "Login über VIDIS",
+      title1: "Anmelden über VIDIS",
       explanation1: "Klicke auf “Anmelden am Schulportal”",
       pictureAlt1: "Vidis Einstieg Visualisierung",
       title2: "Suchen des Schulportals",
@@ -44,13 +44,13 @@ export default {
         "Tippe Deine Schule, Stadt, Bundesland oder Email Adresse in die Suchmakse ein und wähle Dein Schulportal.",
       buttonLabel2: "Teilnehmende Länder, Schulträger und Schulen",
       pictureAlt2: "Vidis Schulportalauswahl Visualisierung",
-      title3: "Login mit Schulportal",
+      title3: "Anmelden über Schulportal",
       explanation3:
         "Tippe nun in der Anmeldemaske Deines Schulportals Deinen Benutzernamen bzw. Email-Adresse und Dein Passwort ein.",
-      pictureAlt3: "Login Seite Visualisierung",
+      pictureAlt3: "Anmeldeseite Visualisierung",
       title4: "Nutzung von Bildungsangeboten",
       explanation4:
-        "Schon bist Du eingeloggt und kannst das Bildungsangebot sicher und einfach nutzen.",
+        "Schon bist Du angemeldet und kannst das Bildungsangebot sicher und einfach nutzen.",
       pictureAlt4: "Bildungsangebot Visualisierung",
     },
   },
