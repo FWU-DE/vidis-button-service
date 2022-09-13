8c72f962dea6a15cf39e827ffb3476f2eb15106d
Marc Wagner <mwagner@intension.de>
2022-09-13 11:19:23 +0200

    VBTN-101: Added UI tests

---
+++ tests/selenium/src/specs/existingIDP.js
@@ -0,0 +1,40 @@
+const testName = "Should have IDPs in droplist";
+
+const {
+  getDriver,
+  navigate_to_website,
+  click_entranceButton,
+  write_searchbox_and_wait_for_droplist,
+  isNotEmptyIdps,
+  droplist_contains,
+  click_firstResult_droplist,
+  click_secureLogin,
+  check_if_redirected,
+  take_screenshot,
+  get_browser_logs,
+  done,
+} = require('../helpers.js');
+
+module.exports = async function() {
+  const driver = await getDriver();
+
+  try {
+    await navigate_to_website(driver);
+    await click_entranceButton(driver);
+    await write_searchbox_and_wait_for_droplist(driver, "rlp");
+    await isNotEmptyIdps(driver);
+    await droplist_contains(driver, "Schulcampus RLP");
+    await click_firstResult_droplist(driver);
+    await click_secureLogin(driver);
+    await check_if_redirected(driver)
+
+    console.log("• PASS - " + testName);
+  } catch(error) {
+    console.log("• FAIL - " + testName);
+    await get_browser_logs(driver);
+    await take_screenshot(driver);
+    throw(error);
+  } finally {
+    done(driver);
+  }
+}
---
+++ tests/selenium/src/specs/loginWithOtherIdp.js
@@ -0,0 +1,34 @@
+const testName = "Should be able to log in with another IDP ";
+
+const {
+  getDriver,
+  navigate_to_website,
+  write_idp,
+  click_log_in_with_another_idp,
+  delete_preselected_idp,
+  write_searchbox_and_wait_for_droplist,
+  take_screenshot,
+  get_browser_logs,
+  done,
+} = require('../helpers.js');
+
+module.exports = async function() {
+  const driver = await getDriver();
+
+  try {
+    await navigate_to_website(driver);
+    await write_idp(driver, "rp-schulcampus-s");
+    await click_log_in_with_another_idp(driver);
+    await delete_preselected_idp(driver, "Schulcampus RLP");
+    await write_searchbox_and_wait_for_droplist(driver, "newIdp")
+
+    console.log("• PASS - " + testName);
+  } catch(error) {
+    console.log("• FAIL - " + testName);
+    await get_browser_logs(driver);
+    await take_screenshot(driver);
+    throw(error);
+  } finally {
+    done(driver);
+  }
+}
