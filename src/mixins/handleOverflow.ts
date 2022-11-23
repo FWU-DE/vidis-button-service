5d77a22ab2626d79198e410f6687750b4f348512
Eric Haemmerle <eric.haemmerle@intension.de>
2022-11-23 17:44:03 +0100

    VBTN-104 fix wierd viewport bug

--- src/mixins/handleOverflow.ts
+++ src/mixins/handleOverflow.ts
@@ -3,6 +3,7 @@ export default {
     return {
       originalOverflowValueBody: "",
       originalOverflowValueHtml: "",
+      originalViewportMetaContent: "",
     };
   },
   mounted() {
@@ -10,6 +11,8 @@ export default {
     this.originalOverflowValueBody = hostBody.style.overflow;
     const hostHtml = document.getElementsByTagName("html")[0];
     this.originalOverflowValueHtml = hostHtml.style.overflow;
+    const viewportMeta = document.querySelector('meta[name="viewport"]');
+    this.originalViewportMetaContent = viewportMeta?.getAttribute("content");
   },
   methods: {
     handleOverflowOfHostpage(hide: boolean) {
@@ -21,6 +24,22 @@ export default {
       hostHtml.style.overflow = hide
         ? "hidden"
         : this.originalOverflowValueHtml;
+      const viewportMeta = document.querySelector('meta[name="viewport"]');
+      const viewportMetaContent = hide
+        ? "width=device-width,initial-scale=1,minimum-scale=1"
+        : this.originalViewportMetaContent;
+      if (viewportMeta && hide)
+        viewportMeta.setAttribute("content", viewportMetaContent);
+      else if (!viewportMeta && hide) {
+        const meta = document.createElement("meta");
+        meta.name = "viewport";
+        meta.content = "viewportMetaContent";
+        document.getElementsByTagName("head")[0].appendChild(meta);
+      } else if (!hide && !this.originalViewportMetaContent && viewportMeta) {
+        viewportMeta.remove();
+      } else if (!hide && this.originalViewportMetaContent && viewportMeta) {
+        viewportMeta.setAttribute("content", viewportMetaContent);
+      }
     },
   },
 };
