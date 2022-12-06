8b6e43a7d57dc0a3cce13e19db02dbda70c2c212
Eric Haemmerle <eric.haemmerle@intension.de>
2022-12-06 12:05:55 +0100

    VBTN-119 reduce bundle size

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
-const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = defineConfig({
   transpileDependencies: true,
-  plugins: [
-    purgecss({
-      content: ["./**/*.html"],
-    }),
-  ],
-  pluginOptions: {
-    webpackBundleAnalyzer: {
-      openAnalyzer: true,
-    },
-  },
+
   chainWebpack: (config) => {
     config.module
       .rule("vue")
