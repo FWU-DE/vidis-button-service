2a03c1d221ea49a77ab635137e66e942ec1c2ea3
Eric Haemmerle <eric.haemmerle@intension.de>
2023-02-13 17:37:50 +0100

    Change Deployment Urls

--- .github/workflows/scripts/uploadLatest.sh
+++ .github/workflows/scripts/uploadLatest.sh
@@ -5,6 +5,6 @@ find * -type f > assets.list
 
 while IFS="" read -r file || [ -n "$file" ]
 do
-    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
-    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://apps.intension.de/nexus/repository/vidis-cdn-snapshots/current-version-suffix/$file;
+    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://apps.intension.de/nexus/repository/vidis-cdn-snapshots/current-version-suffix/
 done < assets.list
