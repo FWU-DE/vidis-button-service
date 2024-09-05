522fad55b69000df460c2e9d703c87e5aa9e2b0a
lprakashintension <86831082+lprakashintension@users.noreply.github.com>
2024-09-05 16:29:57 +0200

    Fix release pipeline (#97)
    * Fix the publishing of the artifacts
    
    * uploading images and fonts pipeline fix
    
    * debug output
    
    * remove this obselete replacements
    
    * fix pipeline for release

--- .github/workflows/scripts/downloadImagesAndFonts.sh
+++ .github/workflows/scripts/downloadImagesAndFonts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir dist
 cd dist
-curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} -O https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-rc/assets.list
+curl -u $CDN_UPLOAD_USER:$CDN_UPLOAD_SECRET -O https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-rc/assets.list
 
 while IFS="" read -r file || [ -n "$file" ]
 do
-    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --create-dirs -o $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-rc/$file;
+    curl -u $CDN_UPLOAD_USER:$CDN_UPLOAD_SECRET --create-dirs -o $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-rc/$file;
 done < assets.list
