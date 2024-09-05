1f889eb86cd3b7668f3f9c26a06a2c2b8d363df5
Lokesh Prakashkumar <lprakash@intension.de>
2024-09-05 14:55:38 +0200

    Fix the publishing of the artifacts

--- .github/workflows/scripts/uploadImagesAndFonts.sh
+++ .github/workflows/scripts/uploadImagesAndFonts.sh
@@ -6,6 +6,6 @@ find * -type f > assets.list
 
 while IFS="" read -r file || [ -n "$file" ]
 do
-    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
-    curl -u ${{ secrets.CDN_UPLOAD_USER }}:${{ secrets.CDN_UPLOAD_SECRET }} --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
+    curl -u $CDN_UPLOAD_USER:$CDN_UPLOAD_SECRET --upload-file $file https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/$file;
+    curl -u $CDN_UPLOAD_USER:$CDN_UPLOAD_SECRET --upload-file assets.list https://fwu-nexus.intension.eu/repository/vidis-cdn-snapshots/current-version-suffix/
 done < assets.list
