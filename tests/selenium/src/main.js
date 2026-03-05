4977a94a2b0b28fe5b418411a5f22f01cae41c28
Eric Haemmerle <eric.haemmerle@intension.de>
2026-03-05 13:40:51 +0100

    remove debugs

--- tests/selenium/src/main.js
+++ tests/selenium/src/main.js
@@ -14,33 +14,7 @@ const tests = [
   async () => loginWithOtherIdp(),
 ];
 
-const testConnectivity = async (url) => {
-  const { Builder, By } = require("selenium-webdriver");
-  const chrome = require("selenium-webdriver/chrome");
-  let driver;
-  try {
-    const chromeOptions = new chrome.Options().addArguments("headless");
-    driver = await new Builder()
-      .forBrowser("chrome")
-      .setChromeOptions(chromeOptions)
-      .build();
-    await driver.get(url);
-    const title = await driver.getTitle();
-    console.log(`> Connectivity test: Loaded ${url}, title:`, title);
-    const h1 = await driver.findElement(By.css("h1")).getText();
-    console.log("> Connectivity test: Found h1:", h1);
-  } catch (e) {
-    console.error("> Connectivity test failed:", e);
-  } finally {
-    if (driver) await driver.quit();
-  }
-};
-
 const parallelTests = async function () {
-  await testConnectivity("https://example.com");
-  await testConnectivity(
-    "https://tp.fwu.saas-dev.aws.intension.eu/?version=1.14.1-snapshot",
-  );
   Promise.all(
     tests.map(async (test) => {
       test();
